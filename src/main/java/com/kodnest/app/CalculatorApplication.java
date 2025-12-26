3782e6031c9f6e2824e9a46d048f51b6d1470144
AkshathaHM <akshathahmohan@gmail.com>
2025-12-26 13:30:14 +0530

    commit

---
+++ src/main/java/com/kodnest/app/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.kodnest.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
